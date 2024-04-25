9dad0e945fbdf9b4f07291deb2c6d6ef4adf7ac2
0DVD0 <161175754+0DVD0@users.noreply.github.com>
2024-04-25 16:03:45 +0300

    played around

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Asus/Desktop/SEMINAR SDA
+# Install script for directory: C:/Users/Asus/Documents/GitHub/SEMINAR_SDA
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Asus/Desktop/SEMINAR SDA/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Asus/Documents/GitHub/SEMINAR_SDA/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
